2f4bbbc86eab870239d28d5ce32da04faecc41f2
saasan <saasan.s2works@hotmail.com>
2013-07-31 04:07:10 +0900

    モバマス道場にレベル順リスト追加

--- mobamas-dojo/main.py
+++ mobamas-dojo/main.py
@@ -6,13 +6,16 @@ import urllib2
 import jinja2
 import codecs
 from datetime import datetime
+import operator
 
 APP_PATH = os.path.dirname(__file__)
-TEMPLATE = 'template.html'
-OUTPUT = 'index.html'
-OUTPUT_PATH = os.path.join(APP_PATH, OUTPUT)
-HEADER_ROWS = 8
-NUMBER_OF_DOJOS = 300
+TEMPLATE_RANK = 'template_rank.html'
+TEMPLATE_LV = 'template_lv.html'
+OUTPUT_RANK = 'index.html'
+OUTPUT_RANK_PATH = os.path.join(APP_PATH, OUTPUT_RANK)
+OUTPUT_LV = 'lv.html'
+OUTPUT_LV_PATH = os.path.join(APP_PATH, OUTPUT_LV)
+NUMBER_OF_DOJOS = 500
 
 class Dojo:
     def __init__(self, lv, rank, id, type, leader):
@@ -30,7 +33,26 @@ def unicode_csv_reader(utf8_csv_data, dialect=csv.excel, **kwargs):
 def format_datetime(value):
     return value.strftime('%Y-%m-%d %H:%M')
 
-url = 'https://spreadsheets.google.com/pub?key=0Aq6mv2GCHMGLdERhLURsdEZwdVFvREdiYjE4Sy1FUnc&output=csv&range=b' + str(HEADER_ROWS + 1) + ':h' + str(HEADER_ROWS + NUMBER_OF_DOJOS)
+def write_html(template, template_values, output_path):
+    jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(APP_PATH))
+    jinja.filters['datetime'] = format_datetime
+    template = jinja.get_template(template)
+
+    try:
+        output = template.render(template_values)
+    except jinja2.TemplateError, e:
+        print e.reason
+        sys.exit()
+
+    f = codecs.open(output_path, 'w', 'utf-8')
+    try:
+        f.write(output)
+    finally:
+        f.close()
+
+url = 'https://spreadsheets.google.com/pub?key=0Aq6mv2GCHMGLdERhLURsdEZwdVFvREdiYjE4Sy1FUnc&output=csv'
+
+print '---- urlopen ----'
 
 try:
     result = urllib2.urlopen(url)
@@ -38,32 +60,40 @@ except urllib2.URLError, e:
     print e.reason
     sys.exit()
 
-dojos =[]
+dojos = []
+
+print '---- unicode_csv_reader ----'
 
 reader = unicode_csv_reader(result)
-for lv, rank, id, type, smart_phone, cheer, leader in reader:
-    dojos.append(Dojo(lv, rank.replace('.', ''), id, type, leader))
+headers = reader.next()
+for unused, lv, rank, id, type, link, cheer, leader, defense, comment, no, lastUpdate, redundancy in reader:
+    if lv != '' and rank != '' and id != '':
+        dojos.append(Dojo(int(lv), rank.replace('.', ''), id, type, leader))
 
-for dojo in dojos:
-    print dojo.lv + ", " + dojo.rank + ", " + dojo.id + ", " + dojo.type + ", " + dojo.leader
+print '---- dojos_rank ----'
+
+dojos_rank = dojos[:NUMBER_OF_DOJOS]
+
+for i, dojo in enumerate(dojos_rank):
+    print str(i) + ': ' + str(dojo.lv) + ', ' + dojo.rank + ', ' + dojo.id + ', ' + dojo.type + ', ' + dojo.leader
 
 template_values = {
-    'dojos': dojos,
+    'dojos': dojos_rank,
     'now': datetime.now()
 }
+write_html(TEMPLATE_RANK, template_values, OUTPUT_RANK_PATH)
 
-jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(APP_PATH))
-jinja.filters['datetime'] = format_datetime
-template = jinja.get_template(TEMPLATE)
+print '---- dojos_lv ----'
 
-try:
-    output = template.render(template_values)
-except jinja2.TemplateError, e:
-    print e.reason
-    sys.exit()
+dojos.sort(key=operator.attrgetter('lv'), reverse=True)
 
-f = codecs.open(OUTPUT_PATH, 'w', 'utf-8')
-try:
-    f.write(output)
-finally:
-    f.close()
+dojos_lv = dojos[:NUMBER_OF_DOJOS]
+
+for i, dojo in enumerate(dojos_lv):
+    print str(i) + ': ' + str(dojo.lv) + ', ' + dojo.rank + ', ' + dojo.id + ', ' + dojo.type + ', ' + dojo.leader
+
+template_values = {
+    'dojos': dojos_lv,
+    'now': datetime.now()
+}
+write_html(TEMPLATE_LV, template_values, OUTPUT_LV_PATH)
