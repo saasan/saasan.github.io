57e1c9d6e904c5c67aa9e0aaf5d1a92d98f97617
saasan <saasan.s2works@hotmail.com>
2014-04-01 00:14:52 +0900

    chihirotataki

---
+++ chihirotataki/data.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// ここに画像の URL を入れると穴から出てくるようになる。
+
+var IMAGE_IDS = [
+  'chihiro0.png',
+  'chihiro1.png',
+  'chihiro2.png',
+  'chihiro3.png',
+  'chihiro4.png',
+  'chihiro5.png'
+];
